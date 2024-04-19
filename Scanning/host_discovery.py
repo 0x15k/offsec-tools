168728010d74ab6388d9aabacfad5bbf5b6a25cc
Yamil La barrera <135279867+0x15k@users.noreply.github.com>
2024-04-19 13:38:12 -0500

    0x15k scanner v1.5

--- Scanning/host_discovery.py
+++ Scanning/host_discovery.py
@@ -4,7 +4,15 @@ import ipaddress
 import signal
 import sys
 
+def signal_handler(sig, frame):
+    print('\nCtrl+C detected. Exiting...')
+    sys.exit(0)
+
 class HostDiscovery:
+    def __init__(self, network=None):
+        self.network = network
+        signal.signal(signal.SIGINT, signal_handler)   
+    
     @staticmethod
     def is_host_active(ip_address):
         if platform.system() == 'Windows':
@@ -14,17 +22,11 @@ class HostDiscovery:
 
         process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         process.wait(timeout=2)
-        if process.returncode == 0:
-            return True
-        else:
-            return False
+        return process.returncode == 0
 
-    def __init__(self, network=None):
-        self.network = network
-
-    def discover_hosts(self):
+    def scan_hosts(self):
         if not self.network:
-            raise ValueError("Network is not provided")
+            raise ValueError("No se proporcionó la red")
 
         hosts = []
         net = ipaddress.ip_network(self.network, strict=False)
@@ -32,21 +34,15 @@ class HostDiscovery:
             ip_address = str(ip)
             if self.is_host_active(ip_address):
                 hosts.append(ip_address)
-                print(f'{ip_address}')  # Aquí se muestra la dirección IP directamente
-        return hosts
-
-    def scan_hosts(self):
-        signal.signal(signal.SIGINT, self.exit_program)
-        hosts = self.discover_hosts()
-        print(f'The following hosts are active in the network {self.network}:')
+                print(ip_address)
+        print(f'Los siguientes hosts están activos en la red {self.network}:')
         for host in hosts:
             print(host)
-
-    def exit_program(self, sig, frame):
-        print('\nCtrl+C detected. Exiting...')
-        sys.exit(0)
 
     @classmethod
     def main(cls, network):
         host_scanner = cls(network)
         host_scanner.scan_hosts()
+
+'''if __name__ == "__main__":
+    HostDiscovery.main("192.168.1.0/24")'''
--- Scanning/port_discovery.py
+++ Scanning/port_discovery.py
@@ -2,6 +2,9 @@ import socket
 import signal
 import sys
 
+def signal_handler(sig, frame):
+        print('\nCtrl+C detectado. Saliendo...')
+        sys.exit(0)  
 class PortScanner:
     def __init__(self, target, start_port, end_port, verbose=None):
         self.target = target
@@ -9,44 +12,50 @@ class PortScanner:
         self.end_port = end_port
         self.verbose = verbose
         self.open_ports = []
-        signal.signal(signal.SIGINT, self.signal_handler)
-
-    def scan_port(self, port):
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(0.5)
-                result = s.connect_ex((self.target, port))
-                if result == 0:
-                    self.open_ports.append(port)
-                    if self.verbose:
-                        print(f"El puerto {port} está abierto.")
-        
-        except socket.error:
-            pass
+        signal.signal(signal.SIGINT, signal_handler)
 
     def scan_ports(self):
         try:
             for port in range(self.start_port, self.end_port + 1):
-                self.scan_port(port)
+                try:
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        s.settimeout(0.5)
+                        result = s.connect_ex((self.target, port))
+                        if result == 0:
+                            self.open_ports.append(port)
+                            if self.verbose:
+                                print(f"El puerto {port} está abierto.")
+                except socket.error:
+                    pass
         except KeyboardInterrupt:
             print('\nEscaneo interrumpido.')
             sys.exit(0)
+        
         if self.open_ports:
             print(f"Los siguientes puertos están abiertos en {self.target}:")
             for port in self.open_ports:
                 print(port)
         else:
             print(f"No se encontraron puertos abiertos en {self.target}.")
-    def signal_handler(self, sig, frame):
-        print('\nCtrl+C detectado. Saliendo...')
-        sys.exit(0)    
+
+    def discover_services(self):
+        if not self.open_ports:
+            print("No hay puertos abiertos para descubrir servicios.")
+            return
+        
+        print("Descubriendo servicios para los puertos abiertos:")
+        for port in self.open_ports:
+            try:
+                service = socket.getservbyport(port)
+                print(f"Puerto {port}: {service}")
+            except OSError:
+                print(f"No se pudo determinar el servicio para el puerto {port}")
 
     @classmethod
     def main(cls, target, start_port, end_port, verbose):
         port_scanner = cls(target, start_port, end_port, verbose)
         port_scanner.scan_ports()
+        port_scanner.discover_services()
 
 if __name__ == "__main__":    
-    '''
-    PortScanner.main("192.168.18.1", 50, 90, verbose=True)
-    '''
+    PortScanner.main("8.8.8.8", 400, 460, verbose=True)
